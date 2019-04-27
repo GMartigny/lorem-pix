848e7462cc840448a99e7659e5d7fa4ca43567df
Guillaume Martigny <guillaume.martigny@gmail.com>
2019-04-27 18:25:58 +0200

    Add clamp to width and height values

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const { createCanvas } = require("canvas");
+const clamp = require("clamp");
 
 const loremPix = (width, height = width, color = `#${Math.random().toString(16).slice(-6)}`) => {
     if (!width) {
         throw RangeError("Lorem-pix function need at least one non-null argument.");
     }
 
-    const canvas = createCanvas(width, height);
+    const w = clamp(width, 1, 1e4);
+    const h = clamp(height, 1, 1e4);
+
+    const canvas = createCanvas(w, h);
     const ctx = canvas.getContext("2d");
 
     ctx.fillStyle = color;
-    ctx.fillRect(0, 0, width, height);
+    ctx.fillRect(0, 0, w, h);
 
     ctx.beginPath();
 
-    ctx.rect(0, 0, width, height);
+    ctx.rect(0, 0, w, h);
     ctx.moveTo(0, 0);
-    ctx.lineTo(width, height);
-    ctx.moveTo(width, 0);
-    ctx.lineTo(0, height);
+    ctx.lineTo(w, h);
+    ctx.moveTo(w, 0);
+    ctx.lineTo(0, h);
     ctx.lineWidth = 8;
     ctx.strokeStyle = "rgba(255, 255, 255, 0.5)";
     ctx.stroke();
