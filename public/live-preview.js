e97bdcc62880535611225fe2df35ec7ca1145460
Guillaume Martigny <guillaume.martigny@gmail.com>
2020-03-18 18:35:42 +0100

    Displayed url is now a link

--- public/live-preview.js
+++ public/live-preview.js
@@ -1,4 +1,4 @@
-const API_URL = "//lorem-pix.now.sh/api";
+const API_URL = "https://lorem-pix.now.sh/api";
 
 const livePreview = (img, width, height, color) => {
     const url = `${API_URL}/${width}/${height}/${encodeURIComponent(color)}`;
